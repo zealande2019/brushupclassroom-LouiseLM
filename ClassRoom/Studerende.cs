3a5b6d7cebc3ef93eea12e32d06fa4d6a499e230
Louise Lytken Petersen Møller <loui142j@edu.easj.dk>
2019-02-03 21:30:31 +0100

    Added "Studerende" with properties and constructor

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public string _name;
+        public int _birthmonth;
+        public int _birthday;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int Birthmonth
+        {
+            get { return _birthmonth; }
+        }
+        public int Birthday
+        {
+            get { return _birthday; }
+        }
+
+        public Studerende(string Name, int Birthmonth, int Birthday)
+        {
+            Name = _name;
+            Birthmonth = _birthmonth;
+            Birthday = _birthday;
+        }
+    }
+}
