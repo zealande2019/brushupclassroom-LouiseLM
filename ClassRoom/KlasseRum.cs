221a3acd23d2e7580dabdc04a8e71b172843b31f
Louise Lytken Petersen Møller <loui142j@edu.easj.dk>
2019-02-03 22:51:58 +0100

    Added "KlasseRum" with properties and constructor

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public string _className;
+        public List<Studerende> _classList;
+        public DateTime _semesterStart;
+
+        public string ClassName
+        {
+            get { return _className; }
+        }
+        public List<Studerende> ClassList
+        {
+            get { return _classList; }
+        }
+        public DateTime SemesterStart
+        {
+            get { return _semesterStart; }
+        }
+
+        public KlasseRum()
+        {
+        }
+    }
+}
